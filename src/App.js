a6e95b30f581ac68d465e0b3ac7c91eb3627b739
gorap50 <gorap.bscsf19@iba-suk.edu.pk>
2023-05-26 16:31:52 +0500

    Hoc

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ClickCounter from './Components/ClickCounter';
+import HoverCounter from './Components/HoverCounter';
+
+class App extends Component{
+  render(){
+  return (
+    <div className="App">
+      <ClickCounter/> 
+      <HoverCounter/>
+    </div>
+  )
+}
+}
+export default App;
